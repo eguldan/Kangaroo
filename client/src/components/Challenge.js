ecb0487533c1bccd4d94444a1642a5a5254589ee
eguldan <eguldan@umich.edu>
2021-10-25 17:13:45 -0400

    completed challenge

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -1,4 +1,59 @@
+import { useState } from 'react';
+
 export default function Challenge() {
+  // useState to define custom state variable and update function since not in a class
+  const [customState, setCustomState] = useState({"totalCost": "", "lowStock": []})
+
+  function getLowStockItems() {
+    let request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:4567/low-stock");
+    request.send();
+    request.onload = () => {
+      if (request.status === 200) {
+        setCustomState({"totalCost": "", "lowStock": JSON.parse(request.response)})
+      } else {
+        console.log(`error ${request.status} ${request.statusText}`)
+      }
+    }
+  }
+
+  function determineReorderCost() {
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:4567/restock-cost");
+    let toSend = [];
+    for (let i in customState.lowStock) {
+      let item = customState.lowStock[i];
+      if (("quantity" in item) && (item.quantity > 0)) {
+        toSend.push({"id": item.id, "quantity": item.quantity});
+      }
+    }
+    request.send(JSON.stringify(toSend));
+    request.onload = () => {
+      if (request.status === 200) {
+        setCustomState({"totalCost": JSON.parse(request.response), "lowStock": customState.lowStock})
+      } else {
+        console.log(`error ${request.status} ${request.statusText}`)
+      }
+    }
+  }
+
+  function ItemRow(props) {
+    // SKU, name, stock, capacity, order
+    const i = props.index;
+
+    function enterQuantity(event) {
+      customState.lowStock[i]["quantity"] = event.target.value;
+    }
+
+    return (<tr>
+      <td>{customState.lowStock[i].id}</td>
+      <td>{customState.lowStock[i].name}</td>
+      <td>{customState.lowStock[i].stock}</td>
+      <td>{customState.lowStock[i].capacity}</td>
+      <td><input type="text" value={customState.lowStock[i].quantity === 0 ? "" : customState.lowStock[i].quantity} onChange={enterQuantity}/></td>
+    </tr>);
+  }
+
   return (
     <>
       <table>
@@ -12,20 +67,17 @@ export default function Challenge() {
           </tr>
         </thead>
         <tbody>
-          {/* 
-          TODO: Create an <ItemRow /> component that's rendered for every inventory item. The component
-          will need an input element in the Order Amount column that will take in the order amount and 
-          update the application state appropriately.
-          */}
+          {
+            customState.lowStock.map(function(item, i) {
+              return <ItemRow index={i}/>
+            })
+          }
+
         </tbody>
       </table>
-      {/* TODO: Display total cost returned from the server */}
-      <div>Total Cost: </div>
-      {/* 
-      TODO: Add event handlers to these buttons that use the Java API to perform their relative actions.
-      */}
-      <button>Get Low-Stock Items</button>
-      <button>Determine Re-Order Cost</button>
+      <div>Total Cost: {customState.totalCost}</div>
+      <button onClick={getLowStockItems}>Get Low-Stock Items</button>
+      <button onClick={determineReorderCost}>Determine Re-Order Cost</button>
     </>
   );
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.io.FileInputStream;
+import java.io.File;
+import java.util.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import com.google.gson.Gson;
+
 import static spark.Spark.*;
+
+// class for storing inventory information
+class Candy {
+    String name;
+    int id;
+    int stock;
+    int capacity;
+
+    public Candy(String name, int id, int stock, int capacity) {
+        this.name = name;
+        this.id = id;
+        this.stock = stock;
+        this.capacity = capacity;
+    }
+}
+
+// class used for parsing restock-cost POST request
+class restockCandy {
+    int id;
+    String quantity; // convert when needed for calculations
+    double minCost = -1;
+}
 
 public class Main {
 
     public static void main(String[] args) {
-
         //This is required to allow GET and POST requests with the header 'content-type'
         options("/*",
                 (request, response) -> {
@@ -20,15 +49,88 @@ public class Main {
         //This is required to allow the React app to communicate with this API
         before((request, response) -> response.header("Access-Control-Allow-Origin", "http://localhost:3000"));
 
-        //TODO: Return JSON containing the candies for which the stock is less than 25% of it's capacity
+
+        // Return JSON containing the candies for which the stock is less than 25% of it's capacity
         get("/low-stock", (request, response) -> {
-            return null;
+            // XLSX file treated as shared resource bc we don't know where inventory gets updated
+            ArrayList<Candy> lowStockCandies = new ArrayList<Candy>();
+
+            File f = new File("resources/Inventory.xlsx");
+            FileInputStream fis = new FileInputStream(f);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next(); // skip header row
+            // should error check in case format of xlsx file has changed, file has been moved / corrupted
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                String name = row.getCell(0).getStringCellValue();
+                int stock = (int)row.getCell(1).getNumericCellValue();
+                int capacity = (int)row.getCell(2).getNumericCellValue();
+                int id = (int)row.getCell(3).getNumericCellValue();
+
+                if (1.0 * stock / capacity < 0.25) {
+                    Candy x = new Candy(name, id, stock, capacity);
+                    lowStockCandies.add(x);
+                }
+            }
+            Gson gson = new Gson();
+            return gson.toJson(lowStockCandies);
         });
 
-        //TODO: Return JSON containing the total cost of restocking candy
+        // Return JSON containing the total cost of restocking candy
         post("/restock-cost", (request, response) -> {
-            return null;
-        });
+            double total_cost = 0;
+            Gson gson = new Gson();
+            Set<Integer> restockIds = new HashSet<Integer>();
+            Map<Integer, restockCandy> toRestock = new HashMap<Integer, restockCandy>();
 
+            // parse request and populate data structures
+            final restockCandy[] parsedRequest = gson.fromJson(request.body(), restockCandy[].class);
+            for (restockCandy candy : parsedRequest) {
+                restockIds.add(candy.id);
+                toRestock.put(candy.id, candy);
+            }
+
+            File f = new File("resources/Distributors.xlsx");
+            FileInputStream fis = new FileInputStream(f);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+            for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+                XSSFSheet sheet = wb.getSheetAt(i);
+                Iterator<Row> rowIterator = sheet.iterator();
+                rowIterator.next(); // skip header row
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+                    // error prevention for sheets with empty rows
+                    if (row.getCell(0) == null) {
+                        break;
+                    }
+                    int id = (int)row.getCell(1).getNumericCellValue();
+                    double cost = row.getCell(2).getNumericCellValue();
+
+                    if (restockIds.contains(id)) {
+                        if (toRestock.get(id).minCost == -1) {
+                            // haven't seen a price for this candy yet
+                            toRestock.get(id).minCost = cost;
+                        }
+                        else if (toRestock.get(id).minCost > cost) {
+                            // cheaper than previously checked distributor
+                            toRestock.get(id).minCost = cost;
+                        }
+                    }
+                }
+            }
+
+            // add up cost of candies
+            Iterator<Integer> it = restockIds.iterator();
+            while (it.hasNext()) {
+                int id = it.next();
+                total_cost += toRestock.get(id).minCost * Integer.parseInt(toRestock.get(id).quantity);
+            }
+
+            // return total cost rounded to 2 decimal points
+            return gson.toJson(Math.round(total_cost * 100.0) / 100.0);
+        });
     }
 }
